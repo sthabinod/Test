0397de674be1ef6cbf71e6034822964ae45171f3
sthabinod <88965570+aanchal-dot@users.noreply.github.com>
2021-09-10 08:57:15 +0545

    binod commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TESTING.wsgi --log-file -
--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g)yv3o1$)x%%%aqco7vj6(!3g$pu24rv98%51h$x&#90hwi)%y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://testing-search-engine.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
